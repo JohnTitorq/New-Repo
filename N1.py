66f73215752310f096fd2912cfccba5ebb73c878
JohnTitor <johntitorqwqqwqqwq@yahoo.com>
2023-08-05 20:25:12 +0300

    init

---
+++ N1.py
@@ -0,0 +1,21 @@
+"""def str_counter(s):
+    for sym in set(s):
+        counter=0
+        for sub_sym in s:
+            if sym==sub_sym: counter+=1
+        print(sym, counter)
+
+str_counter("qwertttyq")"""
+
+
+def str_counter(s):
+    syms_counter={}
+    for sym in s:
+        syms_counter[sym]=syms_counter.get(sym, 0)+1
+    
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+str_counter("qwertttyq")
+
+
