0ebd12b02943ed00167eb66b53c46dd39c576199
JohnTitor <johntitorqwqqwqqwq@yahoo.com>
2023-08-05 21:58:04 +0300

    9:57PM

--- N1.py
+++ N1.py
@@ -7,7 +7,7 @@
 
 str_counter("qwertttyq")"""
 
-
+"""
 def str_counter(s):
     syms_counter={}
     for sym in s:
@@ -18,4 +18,14 @@ def str_counter(s):
 
 str_counter("qwertttyq")
 
+"""
+# Практика, задача 4
 
+def palindromic():
+    Input=input(">>> ")                                  # 1) Ввод строки в любом виде
+    Input=Input.replace(" ", "").lower()                 # 2) Поставить строку в нижний регистр; убрать пробелы
+    Double=len(Input)//2                                 # 3) Разделить строку на 2 строки пополам
+    if Input[:Double]==Input[Double:][::-1]: print(True) # 4) Если первая часть строки (слева) равна перевёрнутой второй части строки (справа), то вывести True 
+    else: print(False)                                   # 5) В противном случае False
+
+palindromic()
